071d0b73fe731e6984514aa9af6021258d34c037
RubyIsCoding <ruby.chaulagain@yahoo.com>
2023-08-11 18:42:14 -0400

    Implemented persistent data storage using SharedPreferences in MainActivity

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.dice
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.*
@@ -9,21 +11,29 @@ import androidx.appcompat.app.AppCompatDelegate
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sharedPreferences: SharedPreferences
+    private val prefsKey = "MyPrefs"
+    private val customValueKey = "CustomValue"
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize SharedPreferences
+        sharedPreferences = getSharedPreferences(prefsKey, Context.MODE_PRIVATE)
+
         var getSpinnerValue = ""
 
         var themeSwitch = findViewById<Switch>(R.id.themeSwitch)
         themeSwitch.setOnCheckedChangeListener { _, onSwitch ->
             if (onSwitch) {
-                themeSwitch.setText("Dark")
-                // on below line we are changing the theme to light mode.
+                themeSwitch.text = "Dark"
+                // here we are changing the theme to light mode.
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             } else {
-                themeSwitch.setText("Light")
-                // on below line we are changing the theme to dark mode.
+                themeSwitch.text = "Light"
+                // here we are changing the theme to dark mode.
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             }
         }
@@ -74,34 +84,48 @@ class MainActivity : AppCompatActivity() {
             when (checkedId) {
                 R.id.radio4sided -> {
                     // Handle the selection of 4-sided die
-                    Toast.makeText(this, "Selected 4-sided die", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "You have selected 4-sided die", Toast.LENGTH_SHORT).show()
                 }
                 R.id.radio6sided -> {
                     // Handle the selection of 6-sided die
-                    Toast.makeText(this, "Selected 6-sided die", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "You have selected 6-sided die", Toast.LENGTH_SHORT).show()
                 }
                 R.id.radio8sided -> {
                     // Handle the selection of 8-sided die
-                    Toast.makeText(this, "Selected 8-sided die", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "You have selected 8-sided die", Toast.LENGTH_SHORT).show()
                 }
                 R.id.radio10sided -> {
                     // Handle the selection of 10-sided die
-                    Toast.makeText(this, "Selected 10-sided die", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "You have selected 10-sided die", Toast.LENGTH_SHORT).show()
                 }
                 R.id.radio12sided -> {
                     // Handle the selection of 12-sided die
-                    Toast.makeText(this, "Selected 12-sided die", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "You have selected 12-sided die", Toast.LENGTH_SHORT).show()
                 }
                 R.id.radio20sided -> {
                     // Handle the selection of 20-sided die
-                    Toast.makeText(this, "Selected 20-sided die", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "You have selected 20-sided die", Toast.LENGTH_SHORT).show()
                 }
             }
         }
 
         // Set the default selected radio button here
         diceTypeRadioGroup.check(R.id.radio4sided)
+
+        // Load and set custom value from SharedPreferences
+        val savedCustomValue = sharedPreferences.getInt(customValueKey, 0)
+        val inputSides = findViewById<EditText>(R.id.inputSides)
+        inputSides.setText(savedCustomValue.toString())
+
+        // Save custom value when the "Add" button is clicked
+        val addButton = findViewById<Button>(R.id.button3)
+        addButton.setOnClickListener {
+            val customValue = inputSides.text.toString().toIntOrNull() ?: 0
+            val editor = sharedPreferences.edit()
+            editor.putInt(customValueKey, customValue)
+            editor.apply()
+
+            Toast.makeText(this, "Your value is saved!: $customValue", Toast.LENGTH_SHORT).show()
+        }
     }
 }
-
-
